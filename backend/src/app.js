c6cdf86f619dd7f8439887d94872ae604f255c8e
Narendradev1008 <darjinarendra1008@gmail.com>
2026-08-26 01:50:36 +0530

    backend orign link change to render

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieparser);
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://resume-check-4qlt.onrender.com/',
   credentials: true
 }))
 
