f86a6b783f8a25bc3e194e533d86e4728ad3e9f2
Narendradev1008 <darjinarendra1008@gmail.com>
2026-08-26 19:54:51 +0530

    checking

--- backend/src/controller/interview.controller.js
+++ backend/src/controller/interview.controller.js
@@ -1,5 +1,26 @@
+const pdfParse=require('pdf-parse');
+const generateInterviewReport=require('../services/ai.services');
+const interviewReportModel= require('../models/interviewReport.model')
+
 async function generateInterViewReportController(req,res){
-  
+   const resumeContent=pdfParse(req.file.buffer);
+   const {selfDescription,jobDescription}=req.body
+
+   const interviewReportByAi=await generateInterviewReport(
+    {resume : resumeContent,selfDescription,jobDescription})
+
+    const interviewReport=interviewReportModel.create({
+        user: req.body.user,
+        resume: resumeContent,
+        selfDescription,
+        jobDescription,
+        ...interviewReportByAi
+    })
+
+    res.status(201).json({
+      message:"interview report genrated succesfully",
+      interviewReport
+    })
 }
 
 module.exports={}
--- backend/src/middleware/file.middleware.js
+++ backend/src/middleware/file.middleware.js
@@ -1,1 +1,10 @@
 const multer=require('multer');
+
+const upload=multer({
+   storage:multer.memoryStorage,
+   limites:{
+    filesize:3*1024*1024 //3MB  
+   }
+})
+
+module.exports=upload;
