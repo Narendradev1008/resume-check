ecfe81a7159a1e46cda023a28fed38258f0e750b
Narendradev1008 <darjinarendra1008@gmail.com>
2026-08-26 22:17:50 +0530

    fileee

--- backend/src/controller/interview.controller.js
+++ backend/src/controller/interview.controller.js
@@ -3,7 +3,14 @@ const generateInterviewReport=require('../services/ai.services');
 const interviewReportModel= require('../models/interviewReport.model')
 
 async function generateInterViewReportController(req,res){
+
+  if(!req.file) {
+      return res.status(400).json({ message: "Resume PDF file is required." });
+  }
+
+
    const resumeContent= await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+
    const {selfDescription,jobDescription}=req.body
 
    const interviewReportByAi=await generateInterviewReport(
