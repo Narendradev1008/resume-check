0a348ce8d00651ecb0e3156a6184fc45bab6d53f
Narendradev1008 <darjinarendra1008@gmail.com>
2026-08-29 11:35:20 +0530

    C:/Program Files/Git/

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cookieparser());
 
 
 app.use(cors({
-  origin: 'https://resume-check-aun1pd2q6-narendradev1008s-projects.vercel.app/', // Your local React / Vite frontend
+  origin: 'https://resume-check-aun1pd2q6-narendradev1008s-projects.vercel.app', // Your local React / Vite frontend
   credentials: true
 }))
 
